891756f0127d6f1ac40d81915bbbfc980350c268
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2024-01-22 16:28:02 +0100

    rajout login et register

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
